5e0010c2ff927fcf21c1794cd02f5954848ca3e1
Angéline <anje.jiro@gmail.com>
2023-01-05 10:25:30 +0100

    Initiation js

---
+++ main.js
@@ -0,0 +1,12 @@
+//let number = prompt('Quel table de multiplication voulez-vous réviser ?');
+//let result;
+
+//for (let i = 0; i <= 10; i++) {
+//    result = i * number;
+//    alert(number + '*' + i + '=' + result);
+//}
+
+
+let firstname = prompt('Rentrez votre nom en minuscule :')
+        firstname = firstname.replaceAll('e', '3')
+alert(firstname)
